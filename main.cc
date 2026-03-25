faa17d12e96814253169311158dd9cced682a8d3
Aman Prasasd <prasadaman2000@gmail.com>
2026-03-25 01:17:20 -0700

    a better example

--- main.cc
+++ main.cc
@@ -2,18 +2,21 @@
 
 #include <unistd.h>
 
+
+/*
+usage: ./app <peer-ip> <peer-port> <server-port> <commit-retrieve-file> <out-file>
+*/
 int main(int argc, char** argv) {
     int peer_port = atoi(argv[2]);
     int server_port = atoi(argv[3]);
 
     Application app(argv[1], peer_port, server_port);
 
-    app.CommitFile("server.cc");
-    app.CommitFile("app.cc");
+    app.CommitFile(argv[4]);
 
     sleep(10);
 
-    app.RetrieveFile("server.cc", "server_copy.cc");
+    app.RetrieveFile(argv[4], argv[5]);
 
     while (true);
 }
