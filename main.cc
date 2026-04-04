faeaa1e9da7aaee207c24cde5a4082b722a549d6
Aman Prasad <prasdaman2000@gmail.com>
2026-04-04 00:53:38 -0700

    testing

--- main.cc
+++ main.cc
@@ -1,12 +1,15 @@
 #include "app.h"
 
 #include <unistd.h>
+#include <iostream>
 
 
 /*
 usage: ./app <peer-ip> <peer-port> <server-port> <commit-retrieve-file> <out-file>
 */
 int main(int argc, char** argv) {
+    std::cout << "Here";
+
     int peer_port = atoi(argv[2]);
     int server_port = atoi(argv[3]);
 
