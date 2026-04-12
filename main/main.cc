7c3c0867d43962b4d130db9bd903e57da4749aae
Aman Prasad <prasdaman2000@gmail.com>
2026-04-12 00:43:07 -0700

    switch to absl flags

--- main/main.cc
+++ main/main.cc
@@ -2,23 +2,33 @@
 
 #include <string>
 
+#include "absl/flags/flag.h"
+#include "absl/flags/parse.h"
 #include "app.h"
+
+ABSL_FLAG(std::string, peer_ip, "", "IP address for peer");
+ABSL_FLAG(int, peer_port, -1, "Port on peer");
+ABSL_FLAG(int, server_port, -1, "Port to start server on");
+ABSL_FLAG(std::string, commit_retrive_file, "",
+          "file to commit and then retrieve");
+ABSL_FLAG(std::string, out_file, "", "file to write retrieved file to");
 
 /*
 usage: ./app <peer-ip> <peer-port> <server-port> <commit-retrieve-file>
 <out-file>
 */
 int main(int argc, char** argv) {
-  int peer_port = atoi(argv[2]);
-  int server_port = atoi(argv[3]);
+  absl::ParseCommandLine(argc, argv);
 
-  Application app(argv[1], peer_port, server_port);
+  Application app(absl::GetFlag(FLAGS_peer_ip), absl::GetFlag(FLAGS_peer_port),
+                  absl::GetFlag(FLAGS_server_port));
 
-  app.CommitFile(argv[4]);
+  app.CommitFile(absl::GetFlag(FLAGS_commit_retrive_file));
 
   sleep(10);
 
-  app.RetrieveFile(argv[4], argv[5]);
+  app.RetrieveFile(absl::GetFlag(FLAGS_commit_retrive_file),
+                   absl::GetFlag(FLAGS_out_file));
 
   while (true);
 }
